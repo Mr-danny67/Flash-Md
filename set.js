45f20524f37298c90f084d13dba8413f8541454c
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-07-05 11:33:17 +0300

    🐐 🔵

--- set.js
+++ set.js
@@ -7,7 +7,7 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>',
+module.exports = { session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>ey...',
     PREFIXE: process.env.PREFIX || "+",
     OWNER_NAME: process.env.OWNER_NAME || "France King",
     OWNER_NUMBER : process.env.OWNER_NUMBER || "254105915061", 
